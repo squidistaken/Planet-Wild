7a2cefd6aa0cd79e9b423f96362f8ae6e38f1b7a
Marcus Persson <marcusper2003@gmail.com>
2023-05-21 19:29:13 +0200

    Refactoring Interaction

--- Assets/UI/Drawing UI/BrushManager.cs
+++ Assets/UI/Drawing UI/BrushManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // Credit: Marcus
 public class BrushManager : MonoBehaviour
@@ -16,9 +18,12 @@ public class BrushManager : MonoBehaviour
 
 	private int layerCount;
 
+	private string selectedAnimal;
+
+	private GameManager gameManager;
+
 	void Update()
 	{
-		// Casting a ray to the plane (one with brush manager)
 		var Ray = drawingCamera.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 
@@ -59,14 +64,14 @@ public class BrushManager : MonoBehaviour
 		}
 	}
 
-	public string animalName = "InsertName";
-
 	public void Save()
 	{
-		StartCoroutine(SaveDrawing(animalName));
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		selectedAnimal = gameManager.animalName;
+
+		StartCoroutine(SaveDrawing(selectedAnimal));
 	}
 
-	// TODO: Save images in their own dedicated folder (or downloads?), and also make sure they can be referenced...
 	private IEnumerator SaveDrawing(string fileName)
 	{
 		yield return new WaitForEndOfFrame(); // Waiting until end of frame to render
@@ -84,26 +89,26 @@ public class BrushManager : MonoBehaviour
 		byte[] byteArray = screenshotTexture.EncodeToPNG();
 
 		string filepath = Application.dataPath + "/MyDrawings";
-		fileName = "/" + fileName + ".png";
+		fileName = "/" + fileName + "Drawing.png";
 
 		// Creating the folder if it doesn't already exist.
 		if (!Directory.Exists(filepath))
 		{
 			Directory.CreateDirectory(Application.dataPath + "/MyDrawings");
 			File.WriteAllBytes(filepath + fileName, byteArray);
+			selectedAnimal = null;
 		}
 		else
 		{
 			File.WriteAllBytes(filepath + fileName, byteArray);
+			selectedAnimal = null;
 		}
-		
 	}
-
-	private GameManager gameManager;
 
 	public void UnloadDrawingUI()
 	{
 		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+		selectedAnimal = null;
 		gameManager.UnloadDrawing();
 	}
 }
