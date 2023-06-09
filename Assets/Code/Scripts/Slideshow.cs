1c40379ae73e3f5792ac088d04f3710ffd01b9b4
StanHanze <119316673+StanHanze@users.noreply.github.com>
2023-06-09 13:26:33 +0200

    added a scene with the slides for the story (#56)

---
+++ Assets/Code/Scripts/Slideshow.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class Slideshow : MonoBehaviour
+{
+
+    public Texture[] imageArray;
+    private int currentImage;
+    public float textPositionX = 1100f;
+    public float textPositionY = 1000f;
+    private GUIStyle guiStyle = new GUIStyle();
+    public int fontSize = 75;
+
+    void OnGUI()
+    {
+
+        int w = Screen.width, h = Screen.height;
+
+        Rect imageRect = new Rect(0, 0, Screen.width, Screen.height);
+
+        GUI.DrawTexture(imageRect, imageArray[currentImage]);
+        guiStyle.fontSize = fontSize;
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(textPositionX, textPositionY, 1, 1), "Klik om door te gaan >>", guiStyle);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentImage = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.GetKey(KeyCode.Space)))
+        {
+            currentImage++;
+
+            if (currentImage >= imageArray.Length)
+            {
+                //Go to next scene
+                SceneManager.LoadScene("TutorialScene");
+            }
+        }
+    }
+}
